bff03fdf83bf3b214b9feab7b7b698b561a87117
po1nt <po1nt@protonmail.ch>
2020-07-14 12:32:33 +1000

    partially generation fix

--- __gen.py
+++ __gen.py
@@ -33,5 +33,24 @@ def recurring_list(size):
 
 def partially_list(size, div):
     pair_size = size // div
+    print("pair_size =", pair_size)
 
-    return [0, 1, 2, 3]
+    data = []
+    if size > div:
+        for i in range(0, size, pair_size):     # (0..39, шаг=1)
+            for k in range(i + pair_size, i, -1):   # (0+1, 0, шаг=-1)
+                data.append(k)
+
+        if len(data) > size:
+            data = data[:-(pair_size)]
+
+        while len(data) < size:
+            for num in range(size)[::-1]:
+                if num not in data:
+                    data.append(num)
+
+    else:
+        print("else")
+        data = [i for i in range(size)]
+
+    return data[::-1]
